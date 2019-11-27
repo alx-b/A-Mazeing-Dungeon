126c2fa79958d1d804feadd6c50a451cae0b8075
Mantas <gusmantas@gmail.com>
2019-11-27 13:22:36 +0100

    Added attributes, toString, and constructors to Item class and its subclasses.

--- src/com/company/Sword.java
+++ src/com/company/Sword.java
@@ -1,13 +1,15 @@
 package com.company;
 
 public class Sword extends Item {
+ private int swordDamage;
 
-    public Sword(String name) {
+    public Sword(String name, int swordDamage) {
         super(name);
+        this.swordDamage = swordDamage;
     }
 
     @Override
     public String toString() {
-        return String.format("You just got a new sword! Sword name: %s", getName());
+        return String.format("You just got a new sword! \n Sword name: %s \n Sword damage: %d", getName(), swordDamage);
     }
 }
