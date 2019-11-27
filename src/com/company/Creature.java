4a694f53044a5ec3e57e2dba8deac2b67206ffc2
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2019-11-27 11:40:58 +0100

    Creature

--- src/com/company/Creature.java
+++ src/com/company/Creature.java
@@ -1,4 +1,25 @@
 package com.company;
 
-public class Creature {
+public abstract class Creature {
+    String name;
+    int health;
+    int damage;
+    int maxHealth;
+
+    //private final int MAX_HEALTH =  200;
+
+    public Creature(String name, int health, int damage, int maxHealth) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+        this.maxHealth = maxHealth;
+    }
+
+    public abstract void getDamage();
+
+
+
+
+
+
 }
