eed8cba78bebeff4e35ba7ff0bed7be379a31a62
alx-b <alexandre.blain@hyhs.se>
2019-12-01 14:55:05 +0100

    Better Actions menu, now handle walls.

--- src/com/company/DungeonGame.java
+++ src/com/company/DungeonGame.java
@@ -11,95 +11,6 @@ public class DungeonGame {
         this.maze = new Maze(hero);
     }
 
-    private boolean isHeroInLastRoom() {
-        return (hero.getRow() == 0 && hero.getCol() == 1);
-    }
-
-    public void start() {
-
-        while (!isHeroInLastRoom()) {
-            this.maze.print(this.hero);
-            this.hero.displayInfo();
-
-            this.maze.displayCurrentRoom(this.hero);
-
-            String choice = menu();
-
-            switch (choice) {
-                case "a":
-                    moveHeroWest();
-                    break;
-                case "w":
-                    moveHeroNorth();
-                    break;
-                case "d":
-                    moveHeroEast();
-                    break;
-                case "s":
-                    moveHeroSouth();
-                    break;
-                default:
-                    System.out.println("Not a valid input!");
-                    break;
-            }
-        }
-        System.out.println("You found the exit!");
-    }
-
-    private String menu() {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("---- Actions ----");
-        System.out.println("Enter a to go West");
-        System.out.println("Enter w to go North");
-        System.out.println("Enter d to go East");
-        System.out.println("Enter s to go South");
-
-        System.out.print("Where do you want to go: ");
-        return scan.nextLine();
-    }
-
-    private void moveHeroWest() {
-        if (this.maze.isARoom(this.hero.westOfHero()) && this.hero.getCol() > 0) {
-            this.maze.removeHeroFromMapAndMaze(this.hero);
-            this.hero.moveWest();
-        }
-    }
-
-    private void moveHeroEast() {
-        if (this.maze.isARoom(this.hero.eastOfHero()) && this.hero.getCol() < this.maze.getMaze()[0].length - 1) {
-            this.maze.removeHeroFromMapAndMaze(this.hero);
-            this.hero.moveEast();
-        }
-    }
-
-    private void moveHeroNorth() {
-        if (this.maze.isARoom(this.hero.northOfHero()) && this.hero.getRow() > 0) {
-            this.maze.removeHeroFromMapAndMaze(this.hero);
-            this.hero.moveNorth();
-        }
-    }
-
-    private void moveHeroSouth() {
-        if (this.maze.isARoom(this.hero.southOfHero()) && this.hero.getRow() < this.maze.getMaze().length - 1) {
-            this.maze.removeHeroFromMapAndMaze(this.hero);
-            this.hero.moveSouth();
-        }
-    }
-
-
-    public void getHeroName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("---- Greetings, Hero! What is your name? ----");
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        this.hero.setName(name);
-    }
-
-    private boolean invalidInput(String choice){
-        return !choice.equals("1") && !choice.equals("2");
-    }
-
     public void showMainMenu() {
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("---- Welcome to the A-Mazeing Dungeon! ----");
@@ -122,5 +33,124 @@ public class DungeonGame {
             }
         }
     }
+
+    private void start() {
+        Scanner scan = new Scanner(System.in);
+        while (!isHeroInLastRoom()) {
+            this.maze.print(this.hero);
+            this.hero.displayInfo();
+
+            this.maze.displayCurrentRoom(this.hero);
+
+            menu();
+            boolean loop = true;
+            while (loop) {
+                System.out.print("Where do you want to go: ");
+                String choice = scan.nextLine();
+                switch (choice) {
+                    case "a":
+                        if (moveHeroWest()) {
+                            loop = false;
+                        }
+                        break;
+                    case "w":
+                        if (moveHeroNorth()){
+                            loop = false;
+                        }
+                        break;
+                    case "d":
+                        if (moveHeroEast()){
+                            loop = false;
+                        }
+                        break;
+                    case "s":
+                        if (moveHeroSouth()){
+                            loop = false;
+                        };
+                        break;
+                    default:
+                        System.out.println("Not a valid input!");
+                        loop = true;
+                        break;
+                }
+            }
+        }
+        System.out.println("You found the exit!");
+    }
+
+    private boolean isHeroInLastRoom() {
+        return (hero.getRow() == 0 && hero.getCol() == 1);
+    }
+
+    private void menu() {
+
+        System.out.println("---- Actions ----");
+        System.out.println("Enter a to go West");
+        System.out.println("Enter w to go North");
+        System.out.println("Enter d to go East");
+        System.out.println("Enter s to go South");
+
+    }
+
+    private boolean moveHeroWest() {
+        if (this.maze.isARoom(this.hero.westOfHero()) && this.hero.getCol() > 0) {
+            this.maze.removeHeroFromMapAndMaze(this.hero);
+            this.hero.moveWest();
+            return true;
+        }
+        else{
+            System.out.println("There is a wall in the way.");
+            return false;
+        }
+    }
+
+    private boolean moveHeroEast() {
+        if (this.maze.isARoom(this.hero.eastOfHero()) && this.hero.getCol() < this.maze.getMaze()[0].length - 1) {
+            this.maze.removeHeroFromMapAndMaze(this.hero);
+            this.hero.moveEast();
+            return true;
+        }
+        else{
+            System.out.println("There is a wall in the way.");
+            return false;
+        }
+    }
+
+    private boolean moveHeroNorth() {
+        if (this.maze.isARoom(this.hero.northOfHero()) && this.hero.getRow() > 0) {
+            this.maze.removeHeroFromMapAndMaze(this.hero);
+            this.hero.moveNorth();
+            return true;
+        }
+        else{
+            System.out.println("There is a wall in the way.");
+            return false;
+        }
+    }
+
+    private boolean moveHeroSouth() {
+        if (this.maze.isARoom(this.hero.southOfHero()) && this.hero.getRow() < this.maze.getMaze().length - 1) {
+            this.maze.removeHeroFromMapAndMaze(this.hero);
+            this.hero.moveSouth();
+            return true;
+        }
+        else{
+            System.out.println("There is a wall in the way.");
+            return false;
+        }
+    }
+
+    public void getHeroName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---- Greetings, Hero! What is your name? ----");
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        this.hero.setName(name);
+    }
+
+    private boolean invalidInput(String choice){
+        return !choice.equals("1") && !choice.equals("2");
+    }
+
 
 }
