52703908dd1df39e998ede27d52cf829b914d017
alx-b <alexandre.blain@hyhs.se>
2019-12-10 09:38:33 +0100

    color enum, special room, better questitem

---
+++ src/com/company/QuestItem.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class QuestItem {
+    private String name;
+    private String description;
+
+    public QuestItem(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
